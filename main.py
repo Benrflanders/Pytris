7b3fadb12dc92d69034ff479ecb2be63e202846b
Ben Flanders <Benrflanders@yahoo.com>
2017-07-16 13:00:04 -0400

    File Setup

---
+++ main.py
@@ -0,0 +1,26 @@
+"""
+Tetirs inspired by the Atari Tetris game
+
+
+Controlled block should fall one frae per second
+Use wasd to move and rotate controlled block
+when the controlled/current block touched the ground or another block:
+    relese control and generate new block
+    if any non controlled block touches the intersect line -> GAME OVER
+
+"""
+
+import background.py as bg
+import playerInfo
+
+var score = 0
+playerSprite = /img/player.png
+
+var running = True
+
+def draw(running):
+
+
+def fall():
+
+
---
+++ ui.py
@@ -0,0 +1,10 @@
+"""
+User interface file
+
+
+
+"""
+
+
+class generateUI(assetsPath):
+    
