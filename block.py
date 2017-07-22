0679f4c137ebd003da65cd48eba11c18d73801da
Benrflanders <benrflanders@yahoo.com>
2017-07-21 21:02:31 -0400

    Began using pyglet, attempted to use pyglet decorators

--- block.py
+++ block.py
@@ -4,4 +4,4 @@ A Block class
 """
 
 class Model(object):
-    
+    block_image = "/assets/SquareBlock.png"
--- tetris.py
+++ tetris.py
@@ -9,20 +9,78 @@ when the controlled/current block touched the ground or another block:
     if any non controlled block touches the intersect line -> GAME OVER
 
 """
+import tetris
+##import block
+##import board
+##import window
+##import playerInfo
+from pyglet import event, app
+from pyglet.window import Window, key
+##from pyglet.app import EventLoop
 
-import background.py as bg
-import playerInfo
 
-var score = 0
-playerSprite = /assets/player.png
+class Window:
+    def __init__(self, x, y, visible=True):
+        global app
+        app = self
+    def event(self, func):
+        self.what_todo = func
+    def run(self):
+        self.what_todo()
+    def set_visible(isVisible=True):
+        visible=isVisible
 
-var running = True
+player_score = 0
+test_block = "/assets/SquareBlock.png"
 
-def draw(running, sprite): ##draw each individual sprite
+running = True
 
+event_loop = app.EventLoop()
+
+##def draw(): ##draw each individual sprite
+    
 
 def fall():
+    print("fall!")
+
+def getNewBlock():
+
+    block = randomBlock()
+    return block
+
+def getNextBlock(): 
+    return test_block
+
+@event_loop.event
+def on_window_close(window):
+    event_loop.exit()
+    return pyglet.event.EVENT_HANDLED
+
+@event_loop.event
+def on_key_press(key):
+    print("on_key_press")
+    
 
 
-if __name__ == "__main__":
-    game = 
+def main():
+    ##while running:
+    ##    draw()
+    currBlock = test_block
+    win = Window(320,640,visible=True)
+    @win.event
+    def on_draw():
+        print("drawing...")
+    
+    """keys = key.KeyStateHandler()
+    if keys[key.SPACE]:
+        print("Space bar pressed!")
+    """
+    win.set_visible()
+    event_loop.run()
+
+
+
+if __name__ == '__main__':
+    main()
+
+
