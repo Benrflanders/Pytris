8233c03f4adb00fc9dd51e4a8a067f1f978be0f6
Benrflanders <benrflanders@yahoo.com>
2017-07-25 16:00:10 -0400

    Structured project and removed errors. tetris.py and block.py now work together

--- block.py
+++ block.py
@@ -1,15 +1,46 @@
 import pyglet
-
+import board #uses the board class to check collisions
 """
 A Block class
 
 """
 
+
 class Block(object):
+    rotation = 0
+    
     image = pyglet.image.load('assets/SquareBlock.png')
 
-    def fall():
-        print("falling")
+    def fall(self, speed):
+        #self.y+=speed
+        print("falling at speed %d" % speed)
 
-    
-    
+    def rotate(self):
+        print("rotating")
+        self.rotation += 90
+
+    #freezes the block at its current location
+    def freeze(self):
+        print("freeze the block")
+
+    def collision_grid(self):
+        #if there is a 1 at any position in the matrix
+        #then check collisions for that location
+        collision_grid = [[0,0,0],
+                          [0,0,0],
+                          [0,0,0]]
+        
+    def create_block():
+        return self
+
+    def isColliding(self):
+        return False
+
+    def check_defeat():
+        return True
+
+    def new_block():
+        return Block()
+
+    #def update(self, dt):
+        
--- tetris.py
+++ tetris.py
@@ -1,60 +1,94 @@
 """
-Tetirs inspired by the Atari Tetris game
+Pytirs inspired by the Atari Tetris game
 
 
-Controlled block should fall one frae per second
-Use wasd to move and rotate controlled block
-when the controlled/current block touched the ground or another block:
+Controlled block should fall one frame per second
+Use the left and right arrow keys to move
+Rotate block with up arrow
+Speed up drop with down arrow
+Space to instantly place the current block
+when the controlled/current block touched the ground or another
+block:
     relese control and generate new block
     if any non controlled block touches the intersect line -> GAME OVER
 
 """
+import math
+import os
+import random
+import sys
+
 import pyglet
 import block
 from pyglet.window import key
+from pyglet.text import Label
 
 
 #create board, blocks,
 #board_image = 
 #block_directory = " "
-board = board.create_board(board_width, board_height)
-block = block.create_block() #will take in a list of blocks
+#board = board.create_board(board_width, board_height)
+
+
+#Get Resources
+image = pyglet.image.load('assets/SquareBlock.png')
+
+
+#each block image will have a set size based on a 3x3 grid 
+
+
 
 #Create main window
-win = pyglet.window.Window(200, 600, caption='Pytris')
+win = pyglet.window.Window(400, 800, caption='Pytris')
+
+curr_block = block.Block()
+
+def game_over():
+    game_over_text = pyglet.text("GAME OVER",
+                                       font_name='Times New Roman',
+                                       font_size=32,
+                                       x = 20, y=20)
+    set_overlay(game_over_text)
 
 
-
-@window.event
+@win.event
 def on_draw():
-    window.clear()
-    label.draw()
-    image.blit(window.,0)
+    win.clear()
+    #image.blit(win,0)
 
-@window.event
+@win.event
 def on_key_press(symbol, modifiers):
     if symbol == key.RIGHT:
         print("Right arrow was pressed")
     elif symbol == key.LEFT:
         print("LEFT")
     elif symbol == key.DOWN:
-        fall(2)
+        curr_block.fall(2)
         print("DOWN")
     elif symbol == key.UP:
         print("UP")
+        curr_block.rotate()
     elif symbol == key.SPACE:
         print("SPACE")
 
-@window.event
+@win.event
 def on_key_release(symbol, modifiers):
     if symbol == key.DOWN:
-        fall(1)
+        curr_block.fall(1)
 
 #Game updater
-def update():
+def update(dt):
     #if player block is touching any blocks below
-
-
+        #player_block.freeze() current block, reset player block
+    if curr_block.isColliding():
+        curr_block.freeze()
+        if curr_block.check_defeat():
+            game_over()
+        else:
+            curr_block.new_block()
+            
+pyglet.clock.schedule_interval(update, 1/60.)
+    
 #Start Game
 
 pyglet.app.run()
