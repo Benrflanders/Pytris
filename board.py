a09f5eb319125c3d522c4aa7654a0b53bb5fc6d3
Benrflanders <benrflanders@yahoo.com>
2017-08-02 19:13:31 -0400

    board drawing is based on window width and height; board.draw() simplified to no inputs

--- board.py
+++ board.py
@@ -1,5 +1,7 @@
 import pyglet
 from pyglet import sprite
+from pyglet.canvas.base import Screen
+from pyglet.window import Window
 from pyglet.gl import *
 
 window = pyglet.window.Window() #window for testing block class
@@ -11,17 +13,24 @@ window = pyglet.window.Window() #window for testing block class
 '   and the game over line
 '''
 class Board():
-    def __init__(self, rows, cols):
+    def __init__(self, rows, cols):        
         self.rows = rows
         self.cols = cols
         #self.create_board(0,0, 4, 4)
+        global win_width
+        global win_height
+        win_width, win_height = window.get_size()
 
     @window.event
-    def draw(self, xpos=0, ypos=0, width=50, height=50):
+    def draw(self):
+        global win_width
+        global win_height
+        width = win_width/self.rows
+        height = win_height/self.cols
         pyglet.gl.glLineWidth(2)
         pyglet.gl.glPointSize(10)
-        x = xpos
-        y = ypos
+        x = 0 #x start position of board
+        y = 0 #y start position of board
         for i in range(self.rows):       
             for j in range(self.cols):
                 pyglet.graphics.draw(4, pyglet.gl.GL_POINTS, ('v2f', [x,y,
